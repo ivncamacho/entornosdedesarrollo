ec2f7bb2ad49118a0ceb644234030bdae093d52f
smrv21-06 <smrv21-06@c16x13.iescierva.net>
2024-02-05 21:02:01 +0100

    Añadimos una clase con 3 atributos de tipo String y la clase principal la cambiamos entera, se modifica el archivo README con la explicacion del programa y cambios

--- Principal.java
+++ Principal.java
@@ -1,13 +1,31 @@
-import java.time.LocalDateTime;
+import java.util.Scanner;
 
-public class Principal{
+public class Principal {
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
-		LocalDateTime datetime = fechaHoraActual();
-		System.out.println(datetime);
+		int numero = 2;
+		
+		Usuario[] usuarios = rellenarUsuarios(numero);
+		
+		for (int i = 0; i < usuarios.length; i++) {
+			System.out.println("USUARIO " + (i+1) + "\n--------");
+			System.out.println("Nombre : " + usuarios[i].nombre + " " +  usuarios[i].apellidos + " \nEmail: " +usuarios[i].email + "\n");
+		}
 	}
-	public static LocalDateTime fechaHoraActual(){
-		LocalDateTime datetime = LocalDateTime.now();
-		return datetime;
+	public static Usuario[] rellenarUsuarios(int numero) {
+		Scanner teclado = new Scanner(System.in);
+		Usuario[] usuarios = new Usuario[numero];
+		for (int i = 0; i < usuarios.length; i++) {
+			Usuario personas = new Usuario();
+			System.out.println("Escribe el nombre del usuario nº" +  (i+1));
+			personas.nombre = teclado.nextLine();
+			System.out.println("Escribe los apellidos del usuario nº" +  (i+1));
+			personas.apellidos = teclado.nextLine();
+			System.out.println("Escribe el email del usuario nº" +  (i+1));
+			personas.email = teclado.nextLine();
+			
+			usuarios[i] = personas;
+		}
+		
+		return usuarios;
 	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # entornosdedesarrollo
 Programa que muestra por pantalla la frase Hola mundo y en la siguiente linea muestra fecha y hora exacta (AAAA-MM-DD) + (HH:MM:SS.Milisegundos)
+
+Se crea de nuevo de 0 el programa principal el que con un array de la clase Usuario creada y que contiene tres atributos de tipo String, en el programa principal se crea un metodo que recibe un numero con el numero de usuarios que se van a introducir y va preguntando nombre apellidos y email respectivamente de cada usuario y luego los muestra
